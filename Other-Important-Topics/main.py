eb1d798ce16a227f6ecee37b9503d56971654400
Divyanshu <divyanshusharma20062515@gmail.com>
2026-06-27 00:02:19 +0530

    added New Codebase

--- Other-Important-Topics/main.py
+++ Other-Important-Topics/main.py
@@ -36,4 +36,39 @@ print(next(generator_instance)) # 0
 print(next(generator_instance)) # 1
 print(next(generator_instance)) # 2   
 
+print()
+print('----------------------------Function Caching-------------------------------------')
+# Function caching is an optimization technique used to store results of expensive function calls so that future calls with the same inputs can be served instantly.
 
+# This avoids:
+
+# Re-computation
+# Unnecessary CPU usage
+# Repeated delays
+
+import functools
+import time
+
+@functools.lru_cache(maxsize=None)
+def expensive_calculation(n):
+    print(f"Computing calculation for {n}...")
+    time.sleep(5)  # Simulating heavy computation
+    return n * 2
+
+
+# First run (no cache)
+print(expensive_calculation(20))
+print(expensive_calculation(2))
+print(expensive_calculation(6))
+
+print("-" * 20)
+
+# Second run (cached values)
+print(expensive_calculation(20))
+print(expensive_calculation(2))
+print(expensive_calculation(6))
+
+print("-" * 20)
+
+# New value (not cached)
+print(expensive_calculation(61))
